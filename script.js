2dd5b0bcaa44c51b1f34d4a931b05299a9061e86
Muskan <102637258+Fun-25@users.noreply.github.com>
2023-04-19 23:01:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // complete the given function
 
 function palindrome(str){
+	str.trim();
+	let n=str.length();
+	let l=0;
+	let r=n-1;
+	let count=0;
 
+	for(let i=0;i<n/2;i++){
+		if(str.charAt(l)==str.charAt(r){
+			count++;
+		})
+	}
+	if(count==n/2)
+		console.log("true");
+	else 
+		console.log("false");
 }
 module.exports = palindrome
